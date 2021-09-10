c14e585f22ebab1d5aa7bd4ed88e2d0228974b43
Capybaradino <hoge@gmail.com>
2021-09-11 02:45:46 +0900

    初回ログイン時ユーザ名ランダム作成実装

--- flask/main.py
+++ flask/main.py
@@ -1,9 +1,28 @@
 from flask import Flask, render_template, request
+import sqlite3
+import names
+import uuid
 app = Flask(__name__)
+
 
 @app.route('/')
 def index():
-    return render_template('index.html', title='Cardgame')
+    con = sqlite3.connect('user.db')
+    cur = con.cursor()
+    email = request.headers.get("X-Forwarded-Email")
+    cur.execute("select nickname from user where email = '" + email + "'")
+    username = ''.join(cur.fetchone())
+    if(username is None):
+        uid = str(uuid.uuid4())
+        username = names.get_last_name()
+        cur.execute("insert into user values ('" + uid +
+                    "','" + email + "','" + username + "')")
+        con.commit()
+
+    con.close()
+
+    return render_template('index.html', title='Cardgame', username=username)
+
 
 @app.route('/chkheaders/')
 def chkheaders():
@@ -16,10 +35,31 @@ def chkheaders():
     headers += "</table>"
     return render_template('chkheaders.html', title='Check Headers', headers=headers)
 
+
+@app.route('/chkusers/')
+def chkusers():
+    headers = "<table border=1>"
+    con = sqlite3.connect('user.db')
+    cur = con.cursor()
+
+    for row in cur.execute('select * from user'):
+        headers += "<tr>"
+        for str in row:
+            headers += "<td>" + str + "</td>"
+        headers += "</tr>"
+        #envs += request.headers.get("Host")
+    headers += "</table>"
+
+    con.close()
+
+    return render_template('chkheaders.html', title='Check Users', headers=headers)
+
+
 @app.route('/hello/<name>')
 def hello(name=None):
     return render_template('hello.html', title='flask test', name=name)
 
-## Omajinai
+
+# Omajinai
 if __name__ == "__main__":
     app.run(debug=True)
