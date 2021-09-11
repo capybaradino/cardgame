90f57d1f6e2d4a1fab97812bc04cea2de0338744
Capybaradino <20378644+capybaradino@users.noreply.github.com>
2021-09-12 00:33:53 +0900

    Admin画面実装

--- flask/card_admin.py
+++ flask/card_admin.py
@@ -1,6 +1,17 @@
 from flask import render_template
 import card_user
+import card_db
 
 
-def card_admin_view():
-    return render_template('admin.html', title='Admin')
+def card_admin_view(sid):
+    userinfo = "<table border=1>"
+    userinfo += "<tr>"
+    userinfo += "<td>" + "Mail Address" + "</td><td>" + \
+        card_db.getemail_fromsid(sid) + "</td>"
+    userinfo += "</tr>"
+    userinfo += "<tr>"
+    userinfo += "<td>" + "Nickname" + "</td><td>" + \
+        card_db.getnickname_fromsid(sid) + "</td>"
+    userinfo += "</tr>"
+    userinfo += "</table>"
+    return render_template('admin.html', title='Admin', userinfo=userinfo)
--- flask/card_db.py
+++ flask/card_db.py
@@ -1,6 +1,60 @@
 import sqlite3
 
 
+def getuser_fromsid(sid):
+    uid = getuid_fromsid(sid)
+    user = getuser_fromuid(uid)
+    return user
+
+
+def getuid_fromsid(sid):
+    con = sqlite3.connect('session.db')
+    cur = con.cursor()
+    cur.execute("select uid from usersession where sid = '" + sid + "'")
+    uid = cur.fetchone()[0]
+    con.close()
+    return uid
+
+
+def getuser_fromuid(uid):
+    con = sqlite3.connect('user.db')
+    cur = con.cursor()
+    cur.execute("select * from user where uid = '" + uid + "'")
+    user = cur.fetchone()
+    con.close()
+    return user
+
+
+def getnickname_fromsid(sid):
+    uid = getuid_fromsid(sid)
+    nickname = getnickname_fromuid(uid)
+    return nickname
+
+
+def getnickname_fromuid(uid):
+    con = sqlite3.connect('user.db')
+    cur = con.cursor()
+    cur.execute("select nickname from user where uid = '" + uid + "'")
+    nickname = cur.fetchone()[0]
+    con.close()
+    return nickname
+
+
+def getemail_fromsid(sid):
+    uid = getuid_fromsid(sid)
+    email = getemail_fromuid(uid)
+    return email
+
+
+def getemail_fromuid(uid):
+    con = sqlite3.connect('user.db')
+    cur = con.cursor()
+    cur.execute("select email from user where uid = '" + uid + "'")
+    email = cur.fetchone()[0]
+    con.close()
+    return email
+
+
 def getsid_fromuid(uid):
     con = sqlite3.connect('session.db')
     cur = con.cursor()
@@ -14,7 +68,7 @@ def getsid_fromsid(sid):
     con = sqlite3.connect('session.db')
     cur = con.cursor()
     cur.execute("select sid from usersession where sid = '" + sid + "'")
-    sid = cur.fetchone()
+    sid = cur.fetchone()[0]
     con.close()
     return sid
 
