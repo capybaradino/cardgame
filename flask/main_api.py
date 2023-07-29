5313c780ba693a1426a9722663c6a771c4d3935e
Capybaradino <20378644+capybaradino@users.noreply.github.com>
2023-07-29 04:43:44 +0000

    RESTAPI仮実装

---
+++ flask/main_api.py
@@ -0,0 +1,19 @@
+from flask import Flask
+import debug
+from flask_restx import Resource, Api
+app = Flask(__name__)
+api = Api(app)
+
+
+@api.route('/api/<sid>/game')
+class Game(Resource):
+    def get(self, sid):
+        return {
+            "sid": sid
+        }
+
+
+# Omajinai
+if __name__ == "__main__":
+    # 自動リロードでエラーが出る場合はVSCodeのBREAKPOINTSの"Uncaught Exceptions"のチェックを外すこと
+    app.run(debug=True)
