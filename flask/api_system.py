66b33aaaf824aa4c245cfd20cb741baf6e2a3ae8
Capybaradino <20378644+capybaradino@users.noreply.github.com>
2023-08-10 14:31:42 +0000

    エラーメッセージ改善

--- flask/api_system.py
+++ flask/api_system.py
@@ -7,12 +7,12 @@ def turnend(sid):
     nickname = card_db.getnickname_fromsid(sid)
     if(playdata.state == "p1turn"):
         if(nickname != playdata.player1.name):
-            return {"error": "illegal session"}, 500
+            return {"error": "illegal session or not your turn"}, 500
         card_db.putgamesession(playdata.gsid, "state", "p2turn")
         nextp = playdata.player2
     else:
         if(nickname != playdata.player2.name):
-            return {"error": "illegal session"}, 500
+            return {"error": "illegal session or not your turn"}, 500
         card_db.putgamesession(playdata.gsid, "state", "p1turn")
         nextp = playdata.player1
     nextp.start_turn()
