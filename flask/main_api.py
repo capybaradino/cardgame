b1c0104a53643f0961da98797a835fcf56f9f6ee
Capybaradino <shun.kameoka@gmail.com>
2023-08-12 01:30:21 +0000

    get API実装

--- flask/main_api.py
+++ flask/main_api.py
@@ -2,6 +2,7 @@ from flask import Flask
 from api_play import api_play_hand
 from api_attack import api_unit_attack
 import api_system
+import api_view
 import debug
 from flask_restx import Resource, Api
 import card_user, card_db
@@ -25,11 +26,33 @@ class Test(Resource):
 class Card_system(Resource):
     def post(self, sid, command):
         sid = card_user.card_checksession(sid)
+
+        # 既存ゲームがあるか確認
+        gsid = card_db.getgsid_fromsid(sid)
+        if(gsid == ""):
+            return {"error": "gamesession is null"}, 403
+
         if(sid is None):
             return {"error": "illegal session"}, 403
         if(command == "turnend"):
             return api_system.turnend(sid)
         return 404
+
+
+@api.route('/view/<sid>')
+class Card_view(Resource):
+    def get(self, sid):
+        sid = card_user.card_checksession(sid)
+        if(sid is None):
+            return {"error": "illegal session"}, 403
+
+        # 既存ゲームがあるか確認
+        gsid = card_db.getgsid_fromsid(sid)
+        if(gsid == ""):
+            return {"error": "gamesession is null"}, 403
+        playview = Play_view(sid)
+
+        return api_view.get(playview)
 
 
 @api.route('/play/<sid>/<card1>/<card2>')
@@ -39,7 +62,7 @@ class Card_play(Resource):
         if(sid is None):
             return {"error": "illegal session"}, 403
         if(card1 is None):
-            return {"error": "card1 is null"}, 401
+            return {"error": "card1 is null"}, 403
 
         # 既存ゲームがあるか確認
         gsid = card_db.getgsid_fromsid(sid)
@@ -49,7 +72,7 @@ class Card_play(Resource):
 
         # Player1(自分のターン)か確認
         if(playview.turnstate != "p1turn"):
-            return {"error": "not in your turn"}, 401
+            return {"error": "not in your turn"}, 403
 
         pattern_hand = r'^hand_[0-9]$'
         pattern_leftboard = r'leftboard_[0-5]'
@@ -60,7 +83,7 @@ class Card_play(Resource):
             # ユニットで攻撃
             return api_unit_attack(sid, playview, card1, card2)
         else:
-            return {"error": "illegal card1"}, 401
+            return {"error": "illegal card1"}, 403
 
 # Omajinai
 if __name__ == "__main__":
