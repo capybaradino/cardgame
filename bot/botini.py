068d61472a36f22c648ae95bae4224885b27d608
Capybaradino <shun.kameoka@gmail.com>
2023-08-13 15:30:48 +0900

    botのサービス起動対応

--- bot/botini.py
+++ bot/botini.py
@@ -2,10 +2,16 @@ import configparser
 import os
 
 def getdebugparam(key):
-    is_file = os.path.isfile("bot.conf")
+    value = os.environ.get('CARD_HOME')
+    conf_path = ""
+    if value is not None:
+        conf_path = value + "/bot/bot.conf"
+    else:
+        conf_path = "bot.conf"
+    is_file = os.path.isfile(conf_path)
     if is_file:
         conf = configparser.ConfigParser()
-        conf.read("bot.conf")
+        conf.read(conf_path)
         section = "param"
         return conf.get(section, key)
     else:
