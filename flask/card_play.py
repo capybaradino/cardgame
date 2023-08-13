55f0dc52d33498923000e2c580840b2dacd086fd
Capybaradino <20378644+capybaradino@users.noreply.github.com>
2023-08-13 10:03:35 +0000

    apply formatter

--- flask/card_play.py
+++ flask/card_play.py
@@ -9,17 +9,17 @@ import card_play_util
 def card_play_get(sid):
     playdata = Playdata(sid)
 
-    if(playdata.stat == "lose"):
+    if (playdata.stat == "lose"):
         return render_template(
             'play_lose.html', title='Lose'
         )
 
-    if(playdata.stat == "win"):
+    if (playdata.stat == "win"):
         return render_template(
             'play_win.html', title='Win'
         )
 
-    if(playdata.stat == "matching"):
+    if (playdata.stat == "matching"):
         return render_template(
             'play_matching.html', title='Matching'
         )
@@ -31,7 +31,7 @@ def card_play_get(sid):
     p1hands = viewdata.p1hand
     i = 0
     for hand in p1hands:
-        if(hand is not None):
+        if (hand is not None):
             p1hand.append(card_play_util.card_createcardhtml(hand, "hand", i))
         else:
             p1hand.append(None)
@@ -40,30 +40,32 @@ def card_play_get(sid):
     p2hand = []
     p2hands = viewdata.p2hand
     for hand in p2hands:
-        if(hand is not None):
+        if (hand is not None):
             p2hand.append(card_play_util.card_createcardhtmlp2())
         else:
             p2hand.append(None)
-    
+
     # 盤面
     p1banmen = []
     i = 0
-    while(i < 6):
+    while (i < 6):
         p1banmen.append(card_play_util.card_createcardhtml(None, "p1board", i))
         i = i + 1
     for board in viewdata.p1board:
-        if(board is not None):
+        if (board is not None):
             i = board.locnum
-            p1banmen[i] = card_play_util.card_createcardhtml(board, "p1board", i)
+            p1banmen[i] = card_play_util.card_createcardhtml(
+                board, "p1board", i)
     p2banmen = []
     i = 0
-    while(i < 6):
+    while (i < 6):
         p2banmen.append(card_play_util.card_createcardhtml(None, "p2board", i))
         i = i + 1
     for board in viewdata.p2board:
-        if(board is not None):
+        if (board is not None):
             i = board.locnum
-            p2banmen[i] = card_play_util.card_createcardhtml(board, "p2board", i)
+            p2banmen[i] = card_play_util.card_createcardhtml(
+                board, "p2board", i)
     # プレイヤー
     p1name = card_play_util.card_radiobutton("p1board", 10)
     p2name = card_play_util.card_radiobutton("p2board", 10)
@@ -71,18 +73,25 @@ def card_play_get(sid):
     filename = card_db.getfilename_fromupname("land")
     centercell = "../uploads/" + filename
 
+    # ターン情報
+    if (viewdata.turnstate == "p1turn"):
+        turnendbutton = '<input type=button value="Turn end" onclick="system_turnend()">'
+    else:
+        turnendbutton = ""
+
     return render_template(
         'play.html', title='Play',
         p1_card=p1hand,
         p1_banmen=p1banmen,
-        p1_name = p1name,
+        p1_name=p1name,
         center_cell=centercell,
         p2_card=p2hand,
         p2_banmen=p2banmen,
-        p2_name = p2name,
+        p2_name=p2name,
         play_data=playdata,
-        viewdata = viewdata
-        )
+        viewdata=viewdata,
+        turnendbutton=turnendbutton
+    )
 
 
 def card_play_post(sid, target, callback):
@@ -93,4 +102,3 @@ def card_play_delete(sid, target, callback):
     playdata = Playdata(sid)
     playdata.gameover(sid)
     return redirect(callback)
-    
