4e733af4bb546cca0df515ad246ddbf79e356d46
Capybaradino <shun.kameoka@gmail.com>
2023-08-13 05:39:46 +0000

    apply formatter

--- flask/api_system.py
+++ flask/api_system.py
@@ -4,16 +4,16 @@ import card_db
 
 def turnend(sid):
     playdata = Playdata(sid)
-    if(playdata.stat == "win" or playdata.stat == "lose"):
+    if (playdata.stat == "win" or playdata.stat == "lose"):
         return {"error": "game was over"}, 403
     nickname = card_db.getnickname_fromsid(sid)
-    if(playdata.state == "p1turn"):
-        if(nickname != playdata.player1.name):
+    if (playdata.state == "p1turn"):
+        if (nickname != playdata.player1.name):
             return {"error": "illegal session or not your turn"}, 500
         card_db.putgamesession(playdata.gsid, "state", "p2turn")
         nextp = playdata.player2
     else:
-        if(nickname != playdata.player2.name):
+        if (nickname != playdata.player2.name):
             return {"error": "illegal session or not your turn"}, 500
         card_db.putgamesession(playdata.gsid, "state", "p1turn")
         nextp = playdata.player1
