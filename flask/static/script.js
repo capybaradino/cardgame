cd912206baedcb6dc6d57b55de926800e207906d
Capybaradino <shun.kameoka@gmail.com>
2023-08-15 13:13:27 +0000

    テストページ

---
+++ flask/static/script.js
@@ -0,0 +1,6 @@
+// カードをプレイするボタンがクリックされたときの処理
+document.getElementById("play-card-button").addEventListener("click", function () {
+    // カードをプレイするためのREST APIリクエストを送信
+    // ここにAPIリクエストのコードを記述
+    console.log("カードをプレイしました。");
+});
