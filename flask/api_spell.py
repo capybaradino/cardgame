4684c82c68d9725bc72ff418ec37368358d43aeb
Capybaradino <20378644+capybaradino@users.noreply.github.com>
2023-09-10 06:20:34 +0000

    死亡時効果サポート３（特技は味方にも撃てるように変更）

--- flask/api_spell.py
+++ flask/api_spell.py
@@ -2,6 +2,7 @@ import re
 from class_playview import Play_view
 from class_playinfo import Card_info
 import card_db
+import api_common_common
 
 
 def api_spell(sid, playview: Play_view, card1, card2):
@@ -36,32 +37,28 @@ def api_spell(sid, playview: Play_view, card1, card2):
             value = int(matches.group())
 
             # 攻撃先確認
-            pattern_p1board = r'rightboard_[0-5]$'   # 盤面
-            pattern_p1leader = r'rightboard_10'  # リーダー
+            pattern_p1board = r'leftboard_[0-5]$'   # 盤面
+            pattern_p1leader = r'leftboard_10'  # リーダー
             pattern_p2board = r'rightboard_[0-5]$'   # 盤面
             pattern_p2leader = r'rightboard_10'  # リーダー
-            # TODO 自ボード、自リーダーへの攻撃
-            if re.match(pattern_p2board, card2):
+            if re.match(pattern_p2board, card2) or re.match(pattern_p1board, card2):
                 # TODO 対象制限の確認
 
                 # ボードの確認
                 pattern = r'[0-5]'
                 number = int(re.findall(pattern, card2)[0])
-                boards = playview.p2board
+                if re.match(pattern_p2board, card2):
+                    boards = playview.p2board
+                else:
+                    boards = playview.p1board
                 objcard2: Card_info
                 objcard2 = boards[number]
                 if (objcard2 is None):
                     return {"error": "unit don't exists in card2"}, 403
                 # ALL OK DB更新
                 # 対象ユニットHP減算
-                dhp = objcard2.dhp - value
-                card_db.putsession(playview.playdata.card_table,
-                                   "cuid", objcard2.cuid,
-                                   "dhp", dhp)
-                if (objcard2.hp_org + dhp <= 0):
-                    card_db.putsession(playview.playdata.card_table,
-                                       "cuid", objcard2.cuid,
-                                       "loc", playview.p2name + "_cemetery")
+                api_common_common.unit_hp_change(
+                    sid, playview, objcard2, value)
                 # カード状態変更
                 card_db.putsession(playview.playdata.card_table,
                                    "cuid", objcard1.cuid,
