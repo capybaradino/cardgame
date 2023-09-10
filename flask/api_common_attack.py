b0064da2efabe64c8ad7e0aa46111001bac861fb
Capybaradino <20378644+capybaradino@users.noreply.github.com>
2023-09-10 06:46:56 +0000

    攻撃時ドロー実装

--- flask/api_common_attack.py
+++ flask/api_common_attack.py
@@ -2,6 +2,7 @@ import re
 from class_playview import Play_view
 from class_playinfo import Card_info
 import card_db
+import api_common_util
 
 
 def api_onattack_before(sid, playview: Play_view, objcard1: Card_info):
@@ -18,7 +19,9 @@ def api_onattack_before(sid, playview: Play_view, objcard1: Card_info):
                 value = int(matches.group())
                 new_attack = objcard1.attack + value
             if "drow" in subeffect:
-                # TODO needleman
-                ret = "OK"
+                if "enemy" in subeffect:
+                    board_self, board_enemy, player_self, player_enemy = api_common_util.get_self_or_enemy(
+                        playview, objcard1)
+                    player_enemy.draw_card()
 
     return "OK", 200, new_attack
