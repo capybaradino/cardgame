4eb48a72fada8e2e2cd5795d88ea49c187bb67dc
Capybaradino <shun.kameoka@gmail.com>
2023-09-10 06:37:38 +0000

    [fix] ニードルマンの攻撃力が不当に＋１

--- flask/api_common_attack.py
+++ flask/api_common_attack.py
@@ -4,19 +4,21 @@ from class_playinfo import Card_info
 import card_db
 
 
-def api_onattack_before(sid, playview: Play_view, objcard1):
+def api_onattack_before(sid, playview: Play_view, objcard1: Card_info):
     effect_array = objcard1.effect.split(",")
     new_attack = objcard1.attack
+    effect: str
     for effect in effect_array:
         if effect.startswith("onattack"):
             # TODO 攻撃時効果のバリエーション実装
-            if "attack" in effect:
+            subeffect = effect.split(":")[1]
+            if "attack" in subeffect:
                 pattern = r"[+-]?\d+"
                 matches = re.search(pattern, effect)
                 value = int(matches.group())
                 new_attack = objcard1.attack + value
-            if "drow" in effect:
-                # TODO mimic
+            if "drow" in subeffect:
+                # TODO needleman
                 ret = "OK"
 
     return "OK", 200, new_attack
