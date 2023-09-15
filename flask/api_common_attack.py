807586c9c4c9ed81949966d1886fcc72c4257508
Capybaradino <20378644+capybaradino@users.noreply.github.com>
2023-09-15 14:49:06 +0000

    [fix] プチアーノンの効果持続

--- flask/api_common_attack.py
+++ flask/api_common_attack.py
@@ -3,25 +3,23 @@ from class_playview import Play_view
 from class_playinfo import Card_info
 import card_db
 import api_common_util
+import api_common_status
 
 
 def api_onattack_before(sid, playview: Play_view, objcard1: Card_info):
     effect_array = objcard1.effect.split(",")
-    new_attack = objcard1.attack
     effect: str
     for effect in effect_array:
         if effect.startswith("onattack"):
             # TODO 攻撃時効果のバリエーション実装
             subeffect = effect.split(":")[1]
             if "attack" in subeffect:
-                pattern = r"[+-]?\d+"
-                matches = re.search(pattern, effect)
-                value = int(matches.group())
-                new_attack = objcard1.attack + value
+                api_common_status.api_common_attack_card(
+                    sid, playview, effect, objcard1)
             if "drow" in subeffect:
                 if "enemy" in subeffect:
                     board_self, board_enemy, player_self, player_enemy = api_common_util.get_self_or_enemy(
                         playview, objcard1)
                     player_enemy.draw_card()
 
-    return "OK", 200, new_attack
+    return "OK", 200
