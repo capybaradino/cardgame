07b6d93dd708c057dc99da175969545b398119ba
Capybaradino <20378644+capybaradino@users.noreply.github.com>
2023-09-16 08:46:05 +0000

    ステルス実装とテンションカード表示少し改善

--- bot/botutil.py
+++ bot/botutil.py
@@ -2,11 +2,17 @@ def search_rightboard(p2board):
     # 相手の盤面を検索
     attack_board = -1
     for card in p2board:
+        # ステルスは除外
+        if ("stealth" in card["status"]):
+            continue
         # 前衛を優先して攻撃
         if (card["location"] < 3):
             attack_board = card["location"]
     if (attack_board < 0):
         for card in p2board:
+            # ステルスは除外
+            if ("stealth" in card["status"]):
+                continue
             # 前衛がいない場合は後衛を攻撃
             attack_board = card["location"]
     # 対象盤面がいない場合はリーダーを攻撃
