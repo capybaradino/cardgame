df9ed6ca1c25a8829a62b3eba3e8b0794b05b6e8
Capybaradino <20378644+capybaradino@users.noreply.github.com>
2023-10-31 14:22:19 +0000

    apply formatter

--- flask/api_play.py
+++ flask/api_play.py
@@ -1,10 +1,10 @@
 import re
 
-from class_playinfo import Card_info
-import card_db
 import api_common_dmg
 import api_common_status
 import api_common_tension
+import card_db
+from class_playinfo import Card_info
 from class_playview import Play_view
 
 
@@ -25,49 +25,54 @@ def onplay_effect(sid, playview: Play_view, effect, card2, card3, isRun):
                 return {"error": "Specify 3rd card"}, 403
             else:
                 ret, scode = api_common_dmg.api_common_dmg(
-                    sid, playview, effect, card3, isRun)
+                    sid, playview, effect, card3, isRun
+                )
     if "attack" in effect:
         if "unit" in effect:
             if card3 is None:
                 return {"error": "Specify 3rd card"}, 403
             else:
                 ret, scode = api_common_status.api_common_attack(
-                    sid, playview, effect, card3, isRun)
+                    sid, playview, effect, card3, isRun
+                )
     if "tension" in effect:
         ret, scode = api_common_tension.api_common_tension(
-            sid, playview, effect, card2, isRun)
+            sid, playview, effect, card2, isRun
+        )
     if "active" in effect:
         ret, scode = api_common_status.api_common_active(
-            sid, playview, effect, card2, isRun)
+            sid, playview, effect, card2, isRun
+        )
     if "leader" in effect and "enemy" in effect:
         ret, scode = api_common_dmg.api_common_dmg(
-            sid, playview, effect, "rightboard_10", isRun)
+            sid, playview, effect, "rightboard_10", isRun
+        )
     return ret, scode
 
 
 def api_play_hand(sid, playview: Play_view, card1, card2, card3):
     # ハンドからカードをプレイ
-    pattern = r'[0-9]'
+    pattern = r"[0-9]"
     number = int(re.findall(pattern, card1)[0])
     hands = playview.p1hand
     objcard1: Card_info
     objcard1 = hands[number]
-    if (objcard1 is None):
+    if objcard1 is None:
         return {"error": "illegal card1 number"}
 
     # ユニットの展開先確認
-    pattern_p1board = r'leftboard_[0-5]'
+    pattern_p1board = r"leftboard_[0-5]"
     if re.match(pattern_p1board, card2):
         # ボードの確認
-        pattern = r'[0-5]'
+        pattern = r"[0-5]"
         number = int(re.findall(pattern, card2)[0])
         boards = playview.p1board
         objcard2 = boards[number]
-        if (objcard2 is not None):
+        if objcard2 is not None:
             return {"error": "unit exists in card2"}
         # MP減算
         remainingmp = playview.p1mp - objcard1.cost
-        if (remainingmp < 0):
+        if remainingmp < 0:
             return {"error": "MP short"}
 
         # 召喚時効果の確認
@@ -76,35 +81,37 @@ def api_play_hand(sid, playview: Play_view, card1, card2, card3):
         effect: str
         for effect in effect_array:
             if effect.startswith("onplay"):
-                ret, scode = onplay_effect(
-                    sid, playview, effect, card2, card3, False)
+                ret, scode = onplay_effect(sid, playview, effect, card2, card3, False)
 
-        if (ret != "OK"):
+        if ret != "OK":
             return ret, scode
 
         # ALL OK DB更新
-        card_db.appendlog(playview.playdata.card_table,
-                          "["+playview.p1name+"]play:" + objcard1.name)
-        card_db.putsession("playerstats",
-                           "name", playview.p1name,
-                           "mp", remainingmp)
-        card_db.putsession(playview.playdata.card_table,
-                           "cuid", objcard1.cuid,
-                           "loc", playview.p1name + "_board")
-        card_db.putsession(playview.playdata.card_table,
-                           "cuid", objcard1.cuid,
-                           "locnum", number)
+        card_db.appendlog(
+            playview.playdata.card_table,
+            "[" + playview.p1name + "]play:" + objcard1.name,
+        )
+        card_db.putsession("playerstats", "name", playview.p1name, "mp", remainingmp)
+        card_db.putsession(
+            playview.playdata.card_table,
+            "cuid",
+            objcard1.cuid,
+            "loc",
+            playview.p1name + "_board",
+        )
+        card_db.putsession(
+            playview.playdata.card_table, "cuid", objcard1.cuid, "locnum", number
+        )
 
         # 召喚時効果の実行
         for effect in effect_array:
             if effect.startswith("onplay"):
-                ret, scode = onplay_effect(
-                    sid, playview, effect, card2, card3, True)
+                ret, scode = onplay_effect(sid, playview, effect, card2, card3, True)
 
         playview = Play_view(sid)
-        if (playview.p1hp <= 0):
+        if playview.p1hp <= 0:
             playview.playdata.gameover(sid)
-        if (playview.p2hp <= 0):
+        if playview.p2hp <= 0:
             playview.playdata.gamewin(sid)
     else:
         return {"error": "illegal card2"}, 403
