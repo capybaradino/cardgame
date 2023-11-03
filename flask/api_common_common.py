a6e2e4c8ce313f39294d2be92b7c0b27149d2dc8
Capybaradino <20378644+capybaradino@users.noreply.github.com>
2023-11-03 02:02:05 +0000

    api_common_dmg統合

--- flask/api_common_common.py
+++ flask/api_common_common.py
@@ -1,9 +1,47 @@
+import re
+
+import api_common_common
 import api_common_dead
 import api_common_util
 import card_db
 from class_playdata import Player
 from class_playinfo import Card_info
 from class_playview import Play_view
+
+
+def api_common_dmg(sid, playview: Play_view, effect, objcard2: Card_info):
+    # HP変化系
+    # TODO ターゲットフィルタ
+    pattern = r"(^.*)_.*"
+    matches = re.search(pattern, effect)
+    target = matches.group(1)
+    pattern = r"[+-]?\d+"
+    matches = re.search(pattern, effect)
+    value = int(matches.group())
+
+    # TODO 対象制限の確認
+    # 対象ユニットHP減算
+    card_db.appendlog(playview.playdata.card_table, "effect->" + objcard2.name)
+    api_common_common.unit_hp_change(sid, playview, objcard2, value)
+
+    return "OK", 200
+
+
+def api_common_dmg_leader(sid, playview: Play_view, effect):
+    # HP変化系
+    # TODO ターゲットフィルタ
+    pattern = r"(^.*)_.*"
+    matches = re.search(pattern, effect)
+    target = matches.group(1)
+    pattern = r"[+-]?\d+"
+    matches = re.search(pattern, effect)
+    value = int(matches.group())
+
+    # リーダーHP減算
+    card_db.appendlog(playview.playdata.card_table, "effect->" + playview.p2name)
+    api_common_common.leader_hp_change(playview.p2, value)
+
+    return "OK", 200
 
 
 def unit_hp_change_multi(sid, playview: Play_view, objcards, values):
