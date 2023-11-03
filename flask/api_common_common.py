20e9172addb6e8b64679b87134459a028c2d1edb
Capybaradino <20378644+capybaradino@users.noreply.github.com>
2023-11-03 07:52:55 +0000

    api_common_deadリファクタ(oneffectに統合)

--- flask/api_common_common.py
+++ flask/api_common_common.py
@@ -1,7 +1,7 @@
+import random
 import re
 
 import api_common_common
-import api_common_dead
 import api_common_status
 import api_common_tension
 import api_common_util
@@ -12,19 +12,80 @@ from class_playview import Play_view
 
 
 def onplay_effect(sid, playview: Play_view, effect, card2, card3, isRun):
+    return _onplay_effect(sid, playview, effect, card2, card3, None, isRun)
+
+
+def onplay_effect_objcard(sid, playview: Play_view, effect, objcard: Card_info, isRun):
+    return _onplay_effect(sid, playview, effect, None, None, objcard, isRun)
+
+
+def _onplay_effect(
+    sid, playview: Play_view, effect, card2, card3, objcard: Card_info, isRun
+):
+    if objcard is not None:
+        (
+            board_self,
+            board_enemy,
+            player_self,
+            player_enemy,
+        ) = api_common_util.get_self_or_enemy(playview, objcard)
     # TODO 召喚時効果のバリエーション実装
     if "drow" in effect:
         if isRun:
-            if "bujutsu" in effect:
-                playview.p1.draw_bujutsucard()
-            elif "spell" in effect:
-                playview.p1.draw_card_spell()
+            if objcard is None:
+                player = playview.p1
             else:
-                playview.p1.draw_card()
+                if "enemy" in effect:
+                    player = player_enemy
+                else:
+                    player = player_self
+            pattern = r"[+-]?\d+"
+            matches = re.search(pattern, effect)
+            value = int(matches.group())
+            i = 0
+            while i < value:
+                if "bujutsu" in effect:
+                    player.draw_bujutsucard()
+                elif "spell" in effect:
+                    player.draw_card_spell()
+                else:
+                    player.draw_card()
+                i = i + 1
         ret = "OK"
         scode = 200
     if "dmg" in effect:
-        if not "leader" in effect:
+        if "random" in effect:
+            pattern = r"[+-]?\d+"
+            matches = re.search(pattern, effect)
+            value = int(matches.group())
+            if "enemy" in effect:
+                index = []
+                i = 0
+                card: Card_info
+                for card in board_enemy:
+                    if card is not None:
+                        # HP=0のユニットは除外
+                        if card.hp + card.dhp > 0:
+                            index.append(i)
+                    i = i + 1
+                leader = len(index)
+                number = random.randrange(len(index) + 1)
+                if number == leader:
+                    card_db.appendlog(
+                        playview.playdata.card_table, "effect->" + player_enemy.name
+                    )
+                    api_common_common.leader_hp_change(player_enemy, value)
+                else:
+                    objcard2 = board_enemy[index[number]]
+                    card_db.appendlog(
+                        playview.playdata.card_table, "effect->" + objcard2.name
+                    )
+                    api_common_common.unit_hp_change(sid, playview, objcard2, value)
+                ret = "OK"
+                scode = 200
+            else:
+                raise Exception("illegal randomdmg effect")
+        elif not "leader" in effect:
             if card3 is None:
                 return {"error": "Specify 3rd card"}, 403
             else:
@@ -48,6 +109,8 @@ def onplay_effect(sid, playview: Play_view, effect, card2, card3, isRun):
         elif "leader" in effect:
             # リーダーHP減算
             ret, scode = api_common_dmg_leader(sid, playview, effect, isRun)
+        else:
+            raise Exception("illegal dmg effect")
     if "attack" in effect:
         if "unit" in effect:
             if card3 is None:
@@ -57,9 +120,14 @@ def onplay_effect(sid, playview: Play_view, effect, card2, card3, isRun):
                     sid, playview, effect, card3, isRun
                 )
     if "tension" in effect:
-        ret, scode = api_common_tension.api_common_tension(
-            sid, playview, effect, card2, isRun
-        )
+        if objcard is None:
+            ret, scode = api_common_tension.api_common_tension(
+                sid, playview, effect, card2, isRun
+            )
+        else:
+            ret, scode = api_common_tension.api_common_tension_objcard(
+                sid, playview, effect, objcard, isRun
+            )
     if "active" in effect:
         ret, scode = api_common_status.api_common_active(
             sid, playview, effect, card2, isRun
@@ -161,8 +229,19 @@ def _unit_hp_change(sid, playview: Play_view, objcard2: Card_info, value, mode):
         )
         card_db.appendlog(playview.playdata.card_table, objcard2.name + " dead")
         # 死亡時効果発動
-        api_common_dead.ondead(sid, playview, objcard2)
+        _ondead_effect(sid, playview, objcard2)
     return
+
+
+def _ondead_effect(sid, playview: Play_view, objcard2: Card_info):
+    effect_array = objcard2.effect.split(",")
+    for effect in effect_array:
+        if effect.startswith("ondead"):
+            # effectからondead:以外の部分を切り出し
+            effectbody = objcard2.effect.split("ondead:")[1]
+            api_common_common.onplay_effect_objcard(
+                sid, playview, effectbody, objcard2, True
+            )
 
 
 def leader_hp_change(player: Player, value):
