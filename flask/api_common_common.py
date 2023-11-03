fe0a079ebce0031cf3c709db56676153e33810b9
Capybaradino <20378644+capybaradino@users.noreply.github.com>
2023-11-03 02:24:31 +0000

    onplay_effect 実装先移動

--- flask/api_common_common.py
+++ flask/api_common_common.py
@@ -2,6 +2,8 @@ import re
 
 import api_common_common
 import api_common_dead
+import api_common_status
+import api_common_tension
 import api_common_util
 import card_db
 from class_playdata import Player
@@ -9,7 +11,61 @@ from class_playinfo import Card_info
 from class_playview import Play_view
 
 
-def api_common_dmg(sid, playview: Play_view, effect, objcard2: Card_info):
+def onplay_effect(sid, playview: Play_view, effect, card2, card3, isRun):
+    # TODO 召喚時効果のバリエーション実装
+    if "drow" in effect:
+        if "bujutsu" in effect:
+            playview.p1.draw_bujutsucard()
+        elif "spell" in effect:
+            playview.p1.draw_card_spell()
+        else:
+            playview.p1.draw_card()
+        ret = "OK"
+        scode = 200
+    if "dmg" in effect:
+        if not "leader" in effect:
+            if card3 is None:
+                return {"error": "Specify 3rd card"}, 403
+            else:
+                # 攻撃先確認
+                pattern_p1board = r"leftboard_[0-5]$"  # 盤面
+                pattern_p1leader = r"leftboard_"  # リーダー
+                pattern_p2board = r"rightboard_[0-5]$"  # 盤面
+                pattern_p2leader = r"rightboard_10"  # リーダー
+                # TODO 自ボード、自リーダーへの攻撃
+                if re.match(pattern_p2board, card3):
+                    # ユニットHP減算
+                    objcard3 = api_common_util.getobjcard(playview, card3)
+                    if objcard3 is None:
+                        return {"error": "unit don't exists in target card"}, 403
+                    ret, scode = api_common_dmg(sid, playview, effect, objcard3, isRun)
+                elif re.match(pattern_p2leader, card3):
+                    # リーダーHP減算
+                    ret, scode = api_common_dmg_leader(sid, playview, effect, isRun)
+                else:
+                    return {"error": "illegal target"}, 403
+    if "attack" in effect:
+        if "unit" in effect:
+            if card3 is None:
+                return {"error": "Specify 3rd card"}, 403
+            else:
+                ret, scode = api_common_status.api_common_attack(
+                    sid, playview, effect, card3, isRun
+                )
+    if "tension" in effect:
+        ret, scode = api_common_tension.api_common_tension(
+            sid, playview, effect, card2, isRun
+        )
+    if "active" in effect:
+        ret, scode = api_common_status.api_common_active(
+            sid, playview, effect, card2, isRun
+        )
+    if "leader" in effect and "enemy" in effect:
+        ret, scode = api_common_dmg(sid, playview, effect, "rightboard_10", isRun)
+    return ret, scode
+
+
+def api_common_dmg(sid, playview: Play_view, effect, objcard2: Card_info, isRun):
     # HP変化系
     # TODO ターゲットフィルタ
     pattern = r"(^.*)_.*"
@@ -20,14 +76,15 @@ def api_common_dmg(sid, playview: Play_view, effect, objcard2: Card_info):
     value = int(matches.group())
 
     # TODO 対象制限の確認
-    # 対象ユニットHP減算
-    card_db.appendlog(playview.playdata.card_table, "effect->" + objcard2.name)
-    api_common_common.unit_hp_change(sid, playview, objcard2, value)
+    if isRun:
+        # 対象ユニットHP減算
+        card_db.appendlog(playview.playdata.card_table, "effect->" + objcard2.name)
+        api_common_common.unit_hp_change(sid, playview, objcard2, value)
 
     return "OK", 200
 
 
-def api_common_dmg_leader(sid, playview: Play_view, effect):
+def api_common_dmg_leader(sid, playview: Play_view, effect, isRun):
     # HP変化系
     # TODO ターゲットフィルタ
     pattern = r"(^.*)_.*"
@@ -37,9 +94,11 @@ def api_common_dmg_leader(sid, playview: Play_view, effect):
     matches = re.search(pattern, effect)
     value = int(matches.group())
 
-    # リーダーHP減算
-    card_db.appendlog(playview.playdata.card_table, "effect->" + playview.p2name)
-    api_common_common.leader_hp_change(playview.p2, value)
+    # TODO 対象制限の確認
+    if isRun:
+        # リーダーHP減算
+        card_db.appendlog(playview.playdata.card_table, "effect->" + playview.p2name)
+        api_common_common.leader_hp_change(playview.p2, value)
 
     return "OK", 200
 
