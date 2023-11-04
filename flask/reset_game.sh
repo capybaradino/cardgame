59ac2f5bc797d6dba4bb3b5a231500da076fcac8
Capybaradino <20378644+capybaradino@users.noreply.github.com>
2023-11-04 04:42:54 +0000

    リセットスクリプトに残存プロセス削除追加

--- flask/reset_game.sh
+++ flask/reset_game.sh
@@ -5,3 +5,8 @@ rm -f game.db
 echo 1 | python3 initialize_card.py
 echo 2 | python3 initialize_card.py
 echo 4 | python3 initialize_db.py
+
+set +eu
+ps aux | grep -e main.py | head -n 1 | sed 's/^.....\ *\([0-9]*\)\ *.*/\1/g' | xargs kill
+ps aux | grep -e main_api.py | head -n 1 | sed 's/^.....\ *\([0-9]*\)\ *.*/\1/g' | xargs kill
+
