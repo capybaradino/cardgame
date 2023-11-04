049df1fd85a675270a5e01077802b9accd439b89
Capybaradino <20378644+capybaradino@users.noreply.github.com>
2023-11-04 12:34:32 +0000

    api_tensionリファクタ

--- flask/test_api_tension.py
+++ flask/test_api_tension.py
@@ -2,6 +2,7 @@ import unittest
 from unittest.mock import Mock, patch
 
 import api_common_common
+import api_common_util
 import api_tension
 import card_db
 
@@ -17,12 +18,14 @@ class TestAPITension(unittest.TestCase):
         self.playview.p1job = "wiz"
         self.playview.p2board = [None, None, None, None]
         self.playview.p2hp = 10
+        self.playview.p2.hp = 10
         self.playview.playdata = Mock()
         self.playview.playdata.card_table = "test_card_table"
         self.playview.playdata.player1 = Mock()
         self.playview.playdata.player1.name = "test_player_1"
-        self.playview.playdata.p1_player_tid = 1
-        self.playview.playdata.p2_player_tid = 2
+        self.playview.playdata.p1_player_tid = "test_player_1"
+        self.playview.playdata.p2_player_tid = "test_player_2"
+        self.playview.p2.player_tid = "test_player_2"
         card_db.getrecord_fromsession = Mock()
         card_db.getrecord_fromsession.return_value = [0, 0, "test_cuid", 0, 0, 0, 1, 0]
         card_db.putsession = Mock()
@@ -62,6 +65,8 @@ class TestAPITension(unittest.TestCase):
         objcard2.refresh = Mock()
         objcard2.status = "test_status"
         objcard2.name = "test_name"
+        api_common_util.getobjcard = Mock()
+        api_common_util.getobjcard.return_value = objcard2
         self.playview.p2board[0] = objcard2
         result = api_tension.api_tension(
             self.sid, self.playview, "card1", "rightboard_0"
@@ -81,17 +86,19 @@ class TestAPITension(unittest.TestCase):
 
     def test_api_tension_with_tension_skill_wiz_leader(self):
         self.playview.p1tension = 3
+        api_common_common.leader_hp_change = Mock()
         result = api_tension.api_tension(
             self.sid, self.playview, "card1", "rightboard_10"
         )
         self.assertEqual(result, {"info": "OK"})
-        card_db.putsession.assert_any_call(
-            "playerstats",
-            "player_tid",
-            self.playview.playdata.p2_player_tid,
-            "hp",
-            7,
-        )
+        # card_db.putsession.assert_any_call(
+        #     "playerstats",
+        #     "player_tid",
+        #     self.playview.p2.player_tid,
+        #     "hp",
+        #     7,
+        # )
+        api_common_common.leader_hp_change.assert_called_once_with(self.playview.p2, 3)
         card_db.appendlog.assert_any_call(
             self.playview.playdata.card_table,
             "[" + self.playview.p1name + "]tension skill:",
@@ -105,25 +112,29 @@ class TestAPITension(unittest.TestCase):
         result = api_tension.api_tension(
             self.sid, self.playview, "card1", "invalid_card"
         )
-        self.assertEqual(result, ({"error": "illegal card2"}, 403))
+        self.assertEqual(result, ({"error": "illegal target"}, 403))
 
     def test_api_tension_with_tension_skill_wiz_unit_dont_exist(self):
         self.playview.p1tension = 3
+        api_common_util.getobjcard = Mock()
+        api_common_util.getobjcard.return_value = None
         result = api_tension.api_tension(
             self.sid, self.playview, "card1", "rightboard_0"
         )
-        self.assertEqual(result, ({"error": "unit don't exists in card2"}, 403))
+        self.assertEqual(result, ({"error": "unit don't exists in target"}, 403))
 
     def test_api_tension_with_tension_skill_wiz_antieffect(self):
         self.playview.p1tension = 3
-        self.playview.p2board[0] = Mock()
-        self.playview.p2board[0].refresh = Mock()
-        self.playview.p2board[0].refresh.return_value = None
-        self.playview.p2board[0].status = ["antieffect"]
+        objcard2 = Mock()
+        objcard2.refresh = Mock()
+        objcard2.status = "antieffect"
+        objcard2.name = "test_name"
+        api_common_util.getobjcard = Mock()
+        api_common_util.getobjcard.return_value = objcard2
         result = api_tension.api_tension(
             self.sid, self.playview, "card1", "rightboard_0"
         )
-        self.assertEqual(result, ({"error": "card2 has antieffect"}, 403))
+        self.assertEqual(result, ({"error": "target unit has antieffect"}, 403))
 
     def test_api_tension_with_tension_skill_mnk(self):
         self.playview.p1tension = 3
