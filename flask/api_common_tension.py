4f9c5309cbdc0948e917dd118ada84ed6a723479
Capybaradino <20378644+capybaradino@users.noreply.github.com>
2023-11-04 10:22:24 +0000

    apply formatter

--- flask/api_common_tension.py
+++ flask/api_common_tension.py
@@ -1,11 +1,20 @@
 import re
-from class_playview import Play_view
-from class_playinfo import Card_info
-import card_db
+
 import api_common_util
+import card_db
+from class_playinfo import Card_info
+from class_playview import Play_view
 
 
-def api_common_tension_objcard(sid, playview: Play_view, effect, objcard2: Card_info, isRun, player_self, player_enemy):
+def api_common_tension_objcard(
+    sid,
+    playview: Play_view,
+    effect,
+    objcard2: Card_info,
+    isRun,
+    player_self,
+    player_enemy,
+):
     if "tension" in effect:
         # 事前チェックは不要
         if not isRun:
@@ -14,24 +23,36 @@ def api_common_tension_objcard(sid, playview: Play_view, effect, objcard2: Card_
         matches = re.search(pattern, effect)
         value = int(matches.group())
         if player_self is None:
-            board_self, board_enemy, player_self, player_enemy = api_common_util.get_self_or_enemy(
-                playview, objcard2)
+            (
+                board_self,
+                board_enemy,
+                player_self,
+                player_enemy,
+            ) = api_common_util.get_self_or_enemy(playview, objcard2)
         if "self" in effect or "each" in effect:
             newvalue = playview.p1tension + value
-            if (newvalue > 3):
+            if newvalue > 3:
                 newvalue = 3
             if isRun:
-                card_db.putsession("playerstats",
-                                   "player_tid", player_self.player_tid,
-                                   "tension", newvalue)
+                card_db.putsession(
+                    "playerstats",
+                    "player_tid",
+                    player_self.player_tid,
+                    "tension",
+                    newvalue,
+                )
         if "enemy" in effect or "each" in effect:
             newvalue = playview.p2tension + value
-            if (newvalue > 3):
+            if newvalue > 3:
                 newvalue = 3
             if isRun:
-                card_db.putsession("playerstats",
-                                   "player_tid", player_enemy.player_tid,
-                                   "tension", newvalue)
+                card_db.putsession(
+                    "playerstats",
+                    "player_tid",
+                    player_enemy.player_tid,
+                    "tension",
+                    newvalue,
+                )
     else:
         raise Exception
 
@@ -40,4 +61,6 @@ def api_common_tension_objcard(sid, playview: Play_view, effect, objcard2: Card_
 
 def api_common_tension(sid, playview: Play_view, effect, card2: str, isRun):
     objcard2 = api_common_util.getobjcard(playview, card2)
-    return api_common_tension_objcard(sid, playview, effect, objcard2, isRun, None, None)
+    return api_common_tension_objcard(
+        sid, playview, effect, objcard2, isRun, None, None
+    )
