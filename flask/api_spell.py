1766e1efbe6a4d35915af2af89bfb24095f1a72c
Capybaradino <20378644+capybaradino@users.noreply.github.com>
2023-11-04 04:45:49 +0000

    スペルのエフェクトをcommon_commonに統合

--- flask/api_spell.py
+++ flask/api_spell.py
@@ -21,140 +21,29 @@ def api_spell(sid, playview: Play_view, card1, card2):
     effect_array = objcard1.effect.split(",")
     # 特技の対象確認
     for effect in effect_array:
-        if "self_1drow" in effect:
-            # MP減算
-            remainingmp = playview.p1mp - objcard1.cost
-            if remainingmp < 0:
-                return {"error": "MP short"}, 403
-            card_db.putsession(
-                "playerstats", "name", playview.p1name, "mp", remainingmp
-            )
-            playview.p1.draw_card()
-            card_db.appendlog(
-                playview.playdata.card_table,
-                "[" + playview.p1name + "]spell:" + objcard1.name,
-            )
+        # 事前チェック
+        ret, scode = api_common_common.onplay_effect_spell(
+            sid, playview, effect, objcard1, card2, False
+        )
+        if ret != "OK":
+            return ret, scode
 
-        if "switch" in effect:
-            # 対象確認
-            pattern_p1board = r"leftboard_[0-5]$"  # 盤面
-            pattern_p2board = r"rightboard_[0-5]$"  # 盤面
-            if re.match(pattern_p2board, card2) or re.match(pattern_p1board, card2):
-                # TODO 対象制限の確認
-                objcard2 = api_common_util.getobjcard(playview, card2)
-                if objcard2 is None:
-                    return {"error": "unit don't exists in card2"}, 403
-                # MP減算
-                remainingmp = playview.p1mp - objcard1.cost
-                if remainingmp < 0:
-                    return {"error": "MP short"}
-                card_db.putsession(
-                    "playerstats", "name", playview.p1name, "mp", remainingmp
-                )
-                # ALL OK DB更新
-                card_db.appendlog(
-                    playview.playdata.card_table,
-                    "[" + playview.p1name + "]spell:" + objcard1.name,
-                )
-                card_db.appendlog(
-                    playview.playdata.card_table, "target->" + objcard2.name
-                )
-                # 対象ユニット場所入れ替え
-                objcard3, loc1, loc2 = api_common_util.getobjcard_oppsite(
-                    playview, card2
-                )
-                card_db.putdeck_locnum(
-                    playview.playdata.card_table, objcard2.cuid, loc2
-                )
-                if objcard3 is not None:
-                    card_db.putdeck_locnum(
-                        playview.playdata.card_table, objcard3.cuid, loc1
-                    )
+        # MP減算確認
+        remainingmp = playview.p1mp - objcard1.cost
+        if remainingmp < 0:
+            return {"error": "MP short"}, 403
 
-            else:
-                return {"error": "illegal card2"}, 403
-
-        if "dmg" in effect:
-            # HP変化系
-            pattern = r"(^.*)_.*"
-            matches = re.search(pattern, effect)
-            target = matches.group(1)
-            pattern = r"\d+"
-            matches = re.search(pattern, effect)
-            value = int(matches.group())
-
-            # 攻撃先確認
-            pattern_p1board = r"leftboard_[0-5]$"  # 盤面
-            pattern_p1leader = r"leftboard_10"  # リーダー
-            pattern_p2board = r"rightboard_[0-5]$"  # 盤面
-            pattern_p2leader = r"rightboard_10"  # リーダー
-            if re.match(pattern_p2board, card2) or re.match(pattern_p1board, card2):
-                # TODO 対象制限の確認
-                objcard2 = api_common_util.getobjcard(playview, card2)
-                if objcard2 is None:
-                    return {"error": "unit don't exists in card2"}, 403
-
-                # 特技無効チェック
-                if card2 is not None:
-                    if "antieffect" in objcard2.status:
-                        return {"error": "card2 has antieffect"}, 403
-
-                # MP減算
-                remainingmp = playview.p1mp - objcard1.cost
-                if remainingmp < 0:
-                    return {"error": "MP short"}, 403
-                card_db.putsession(
-                    "playerstats", "name", playview.p1name, "mp", remainingmp
-                )
-                # ALL OK DB更新
-                card_db.appendlog(
-                    playview.playdata.card_table,
-                    "[" + playview.p1name + "]spell:" + objcard1.name,
-                )
-                card_db.appendlog(
-                    playview.playdata.card_table, "target->" + objcard2.name
-                )
-                # 対象ユニットHP減算
-                api_common_common.unit_hp_change(sid, playview, objcard2, value)
-
-            elif re.match(pattern_p2leader, card2):
-                # MP減算
-                remainingmp = playview.p1mp - objcard1.cost
-                if remainingmp < 0:
-                    return {"error": "MP short"}
-                card_db.putsession(
-                    "playerstats", "name", playview.p1name, "mp", remainingmp
-                )
-                # ALL OK DB更新
-                card_db.appendlog(
-                    playview.playdata.card_table,
-                    "[" + playview.p1name + "]spell:" + objcard1.name,
-                )
-                card_db.appendlog(
-                    playview.playdata.card_table, "target->" + playview.p2name
-                )
-                # リーダーHP減算
-                newhp = playview.p2hp - value
-                if playview.playdata.player1.name == playview.p1name:
-                    card_db.putsession(
-                        "playerstats",
-                        "player_tid",
-                        playview.playdata.p2_player_tid,
-                        "hp",
-                        newhp,
-                    )
-                else:
-                    card_db.putsession(
-                        "playerstats",
-                        "player_tid",
-                        playview.playdata.p1_player_tid,
-                        "hp",
-                        newhp,
-                    )
-                if newhp <= 0:
-                    playview.playdata.gamewin(sid)
-            else:
-                return {"error": "illegal card2"}, 403
+        # ALL OK DB更新
+        # 監査ログ
+        card_db.appendlog(
+            playview.playdata.card_table,
+            "[" + playview.p1name + "]spell:" + objcard1.name,
+        )
+        # MP減算
+        card_db.putsession("playerstats", "name", playview.p1name, "mp", remainingmp)
+        ret, scode = api_common_common.onplay_effect_spell(
+            sid, playview, effect, objcard1, card2, True
+        )
 
     # カード状態変更
     card_db.putsession(
