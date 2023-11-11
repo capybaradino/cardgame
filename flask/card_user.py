3b282284ca1dc02afe89791ae676f790fe078b2c
Capybaradino <20378644+capybaradino@users.noreply.github.com>
2023-11-11 08:37:18 +0000

    aplly formatter (システム系)

--- flask/card_user.py
+++ flask/card_user.py
@@ -7,9 +7,11 @@ import re
 
 
 def card_checksession(sid):
-    pattern_session = r"^[0-9a-fA-F]{8}-[0-9a-fA-F]{4}-[0-9a-fA-F]{4}-[0-9a-fA-F]{4}-[0-9a-fA-F]{12}$"
+    pattern_session = (
+        r"^[0-9a-fA-F]{8}-[0-9a-fA-F]{4}-[0-9a-fA-F]{4}-[0-9a-fA-F]{4}-[0-9a-fA-F]{12}$"
+    )
     if re.match(pattern_session, sid):
-        if (sid is not None):
+        if sid is not None:
             sid = card_db.getsid_fromsid(sid)
         return sid
     else:
@@ -18,11 +20,11 @@ def card_checksession(sid):
 
 def card_getsession(sid, email):
     # Check email for security
-    if (sid is not None):
+    if sid is not None:
         sid = card_db.getsid_fromsid(sid)
-        if (sid is not None):
+        if sid is not None:
             email_chk = card_db.getemail_fromsid(sid)
-            if (email != email_chk):
+            if email != email_chk:
                 sid = None
     return sid
 
@@ -33,7 +35,7 @@ def card_postsession(uid):
     while True:
         sid = str(uuid.uuid4())
         sid_chk = card_db.getsid_fromsid(sid)
-        if (sid_chk is not None):
+        if sid_chk is not None:
             continue
         break
     datestr = card_util.card_getdatestrnow()
@@ -44,7 +46,7 @@ def card_postsession(uid):
 def card_putsession(sid, uid):
     nowstr = card_util.card_getdatestrnow()
     sid_chk = card_db.getsid_fromsid(sid)
-    if (sid_chk is None):
+    if sid_chk is None:
         # card_db.postusersession(sid, uid, nowstr)
         # TODO エラーケース
         return
@@ -65,23 +67,23 @@ def card_getgrant_fromsid(sid):
 def card_auth(email):
     username = None
     username = card_db.getnickname_fromemail(email)
-    if (username is None):
+    if username is None:
         while True:
             uid = str(uuid.uuid4())
             uid_chk = card_db.getuid_fromuid(uid)
-            if (uid_chk is not None):
+            if uid_chk is not None:
                 greetings = "Sorry, please reload page and try again."
                 break
             username = names.get_last_name()
             username_chk = card_db.getnickname_fromnickname(username)
-            if (username_chk is not None):
+            if username_chk is not None:
                 greetings = "Sorry, please reload page and try again."
                 break
             card_db.postuser(uid, email, username)
             greetings = "Hello, " + username + "! Your name was made randomly."
             break
     else:
-        username = ''.join(username)
+        username = "".join(username)
         uid = card_db.getuid_fromemail(email)
         greetings = "Hello, " + username + "!"
 
@@ -89,4 +91,4 @@ def card_auth(email):
 
 
 def card_cleargame(sid):
-    card_db.putusersession_gsid(sid, '')
+    card_db.putusersession_gsid(sid, "")
