bc857c0cbb58af12f35d56368cc83575ecc9f142
Capybaradino <20378644+capybaradino@users.noreply.github.com>
2023-11-11 08:30:16 +0000

    apply formatter (API系、未実施ファイル)

--- flask/api_system.py
+++ flask/api_system.py
@@ -11,22 +11,20 @@ def surrender(sid):
 
 def turnend(sid):
     playdata = Playdata(sid)
-    if (playdata.stat == "win" or playdata.stat == "lose"):
+    if playdata.stat == "win" or playdata.stat == "lose":
         return {"error": "game was over"}, 403
     nickname = card_db.getnickname_fromsid(sid)
-    if (playdata.state == "p1turn"):
-        if (nickname != playdata.player1.name):
+    if playdata.state == "p1turn":
+        if nickname != playdata.player1.name:
             return {"error": "illegal session or not your turn"}, 500
         card_turnend.card_turnend(sid, "p1turn", nickname)
-        card_db.appendlog(playdata.card_table, "=" +
-                          nickname + " end=")
+        card_db.appendlog(playdata.card_table, "=" + nickname + " end=")
         nextp = playdata.player2
     else:
-        if (nickname != playdata.player2.name):
+        if nickname != playdata.player2.name:
             return {"error": "illegal session or not your turn"}, 500
         card_turnend.card_turnend(sid, "p2turn", nickname)
-        card_db.appendlog(playdata.card_table, "=" +
-                          nickname + " end=")
+        card_db.appendlog(playdata.card_table, "=" + nickname + " end=")
         nextp = playdata.player1
     nextp.start_turn()
     return {"info": "OK"}
